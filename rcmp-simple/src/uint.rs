4f3627e570f82961ed6980feafd7bd1f778b2d84
Kneelawk <kneelawk@gmail.com>
2021-12-17 11:32:07 -0800

    Clean up doc commends and add Sub implementation

--- rcmp-simple/src/uint.rs
+++ rcmp-simple/src/uint.rs
@@ -18,7 +18,7 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-use std::ops::Add;
+use std::ops::{Add, Sub};
 
 /// Unsigned integer extended fixed precision implementation.
 ///
@@ -42,7 +42,7 @@ pub struct UInt<const PRECISION: usize> {
 }
 
 impl<const PRECISION: usize> UInt<PRECISION> {
-    /// Creates a new natural number with the given limbs.
+    /// Creates a new unsigned integer with the given limbs.
     ///
     /// Numbers in the limbs are stored with the most significant limb first (at
     /// the smallest index) and the least significant limb last (at the largest
@@ -51,8 +51,8 @@ impl<const PRECISION: usize> UInt<PRECISION> {
         UInt { limbs }
     }
 
-    /// Adds `self` and `rhs` returning a new natural number that is the sum of
-    /// these two numbers.
+    /// Adds `self` and `rhs`, returning a new unsigned integer that is the sum
+    /// of these two numbers.
     ///
     /// # Example
     /// ```rust
@@ -67,19 +67,58 @@ impl<const PRECISION: usize> UInt<PRECISION> {
         // This implementation is practically identical to the one in GMP.
         let mut limbs = [0u32; PRECISION];
         let mut carry = 0u32;
-
-        let (mut a, mut b, mut mid, mut res);
+        let (mut start, mut mid, mut res): (u32, u32, u32);
 
         for i in (0..PRECISION).rev() {
-            a = self.limbs[i];
-            b = rhs.limbs[i];
-            mid = a.wrapping_add(b);
+            start = self.limbs[i];
+            mid = start.wrapping_add(rhs.limbs[i]);
             res = mid.wrapping_add(carry);
-            carry = (mid < a) as u32 | (res < mid) as u32;
+            carry = (mid < start) as u32 | (res < mid) as u32;
             limbs[i] = res;
         }
 
         (UInt { limbs }, carry != 0)
+    }
+
+    /// Subtracts `rhs` from `self`, returning a new unsigned integer that is
+    /// the difference between these two numbers.
+    ///
+    /// The resulting `bool` is true if an underflow occurred.
+    ///
+    /// # Examples
+    /// ```rust
+    /// # use rcmp_simple::UInt;
+    /// let num = UInt::new([1, 0]);
+    /// let (dif, underflow) = num.overflowing_sub(&UInt::new([0, 1]));
+    ///
+    /// assert_eq!(dif, UInt::new([0, 0xFFFFFFFF]));
+    /// assert!(!underflow);
+    /// ```
+    ///
+    /// An example of when an underflow might occur:
+    /// ```rust
+    /// # use rcmp_simple::UInt;
+    /// let num = UInt::new([0, 1]);
+    /// let (dif, underflow) = num.overflowing_sub(&UInt::new([0, 2]));
+    ///
+    /// assert_eq!(dif, UInt::new([0xFFFFFFFF, 0xFFFFFFFF]));
+    /// assert!(underflow);
+    /// ```
+    pub fn overflowing_sub(&self, rhs: &UInt<PRECISION>) -> (UInt<PRECISION>, bool) {
+        // This implementation is practically identical to the one in GMP.
+        let mut limbs = [0u32; PRECISION];
+        let mut borrow = 0u32;
+        let (mut start, mut mid, mut res): (u32, u32, u32);
+
+        for i in (0..PRECISION).rev() {
+            start = self.limbs[i];
+            mid = start.wrapping_sub(rhs.limbs[i]);
+            res = mid.wrapping_sub(borrow);
+            borrow = (mid > start) as u32 | (res > mid) as u32;
+            limbs[i] = res;
+        }
+
+        (UInt { limbs }, borrow != 0)
     }
 }
 
@@ -94,6 +133,21 @@ impl<const PRECISION: usize> Add for UInt<PRECISION> {
     fn add(self, rhs: Self) -> Self::Output {
         let (res, overflow) = self.overflowing_add(&rhs);
         debug_assert!(!overflow, "Add overflowed");
+        res
+    }
+}
+
+impl<const PRECISION: usize> Sub for UInt<PRECISION> {
+    type Output = Self;
+
+    /// Performs the `-` operation.
+    ///
+    /// # Panics
+    /// This function panics if an underflow occurs and debug assertions are
+    /// enabled. Otherwise, this function will wrap.
+    fn sub(self, rhs: Self) -> Self::Output {
+        let (res, underflow) = self.overflowing_sub(&rhs);
+        debug_assert!(!underflow, "Subtract underflowed");
         res
     }
 }
@@ -132,5 +186,25 @@ mod tests {
         let sum = num + UInt::new([0, 0xFFFFFFFF, 0xFFFFFFFF]);
 
         assert_eq!(sum, UInt::new([1, 0, 0]), "The result must be [1, 0, 0]");
+    }
+
+    #[test]
+    fn normal_sub() {
+        let num = UInt::new([0, 3]);
+        let dif = num - UInt::new([0, 2]);
+
+        assert_eq!(dif, UInt::new([0, 1]));
+    }
+
+    #[test]
+    fn multi_precision_borrow_sub() {
+        let num = UInt::new([1, 0, 0]);
+        let dif = num - UInt::new([0, 0, 1]);
+
+        assert_eq!(
+            dif,
+            UInt::new([0, 0xFFFFFFFF, 0xFFFFFFFF]),
+            "The result should be [0, 0xFFFFFFFF, 0xFFFFFFFF]"
+        );
     }
 }
